c992548c614e5c04cd9f2dc681d16d24452c125e
홍기 <@JHG>
2015-05-18 11:49:32 +0900

    Snack, SnackTest

--- JavaPractice/src/org/dimigo/oop/SnackTest.java
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -14,7 +14,7 @@ package org.dimigo.oop;
 
 public class SnackTest {
 	public static void main(String[] args){
-		int cost=0;
+		int sum=0;
 		
 		Snack[] snack = new Snack[]{
 				new Snack("새우깡","농심",1100,2),
@@ -25,10 +25,10 @@ public class SnackTest {
 	
 		for(Snack s : snack) {
 			s.printSnack();
-			cost += s.calcPrice();
+			sum += s.calcPrice();
 		}
 		
-		System.out.println("총 구매 금액 : "+String.format("%,d",cost)+"원");
+		System.out.println("총 구매 금액 : "+String.format("%,d",sum)+"원");
 		
 	}
 }
