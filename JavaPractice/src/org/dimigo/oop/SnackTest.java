32823b35215412ebe71554b1bb0cfa8cadc9e25a
홍기 <@JHG>
2015-05-18 11:45:23 +0900

    Snack, SnackTest

---
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -0,0 +1,34 @@
+/**
+ * <pre>
+ * org.dimigo.oop
+ *    |_ SnackTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 5. 18.
+ * </pre>
+ *
+ * @author				: 조홍기
+ * @version			: 1.0
+ */
+package org.dimigo.oop;
+
+public class SnackTest {
+	public static void main(String[] args){
+		int value=0;
+		
+		Snack[] snack = {
+				new Snack("새우깡","농심",1100,2),
+				new Snack("콘칲","크라운",1200,1),
+				new Snack("허니버터칩","해태",1500,4)
+		};
+		
+	
+		for(Snack s : snack) {
+			s.printSnack();
+			value += s.calcPrice();
+		}
+		
+		System.out.println("총 구매 금액 : "+String.format("%,d",value)+"원");
+		
+	}
+}
