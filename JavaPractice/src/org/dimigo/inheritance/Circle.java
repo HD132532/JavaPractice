435c3f09d5babea5bdbf130f0acc9f84e5ffc9a9
홍기 <@JHG>
2015-08-17 12:44:39 +0900

    Figure

--- JavaPractice/src/org/dimigo/inheritance/Circle.java
+++ JavaPractice/src/org/dimigo/inheritance/Circle.java
@@ -28,4 +28,9 @@ public class Circle extends Figure {
 	protected double calcArea(){
 		return radius*radius*3.14;
 	}
+	
+	protected void printCenter(){
+		System.out.print("원 ");
+		super.printCenter();
+	}
 }
