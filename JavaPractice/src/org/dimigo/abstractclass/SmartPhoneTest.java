b9376b594981d39c7cf083bb075660dec2082039
홍기 <@JHG>
2015-09-01 10:16:02 +0900

    abstract

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,32 @@
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *    |_ SmartPhoneTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 8. 25.
+ * </pre>
+ *
+ * @author				: 조홍기
+ * @version			: 1.0
+ */
+package org.dimigo.abstractclass;
+
+public class SmartPhoneTest {
+	public static void main(String[] args){
+		SmartPhone[] phones = {
+				new IPhone("iPhone", "애플", 700000),
+				new Galaxy("갤럭시 S6", "삼성", 650000)
+		};
+		
+		for(SmartPhone p : phones){
+			System.out.println(p.toString());
+			p.turnOn();
+			p.pay();
+			p.useSpecialFunction();
+			p.turnOff();
+			System.out.println();
+		}
+		
+	}
+}
